3266d3b0ddf11f95c8c32748d30e75cd4c4bcb90
银河线 <128875843+GalacticLine@users.noreply.github.com>
2023-09-24 10:13:23 +0800

    Update ga_test.py

--- Test/ModelTest/ModernTest/ga_test.py
+++ Test/ModelTest/ModernTest/ga_test.py
@@ -16,3 +16,19 @@ ga = GeneticAlgorithm(fitness_function,
 
 best_sol, best_fitness = ga.fit()
 print("最优解: x =", best_sol, ", f(x) =", best_fitness)  # 打印最优解
+
+
+def fitness_function(x):
+    """ 适应度函数，求最小化问题时，添加负号 """
+    return - x ** 2
+
+
+ga = GeneticAlgorithm(fitness_function,
+                      bounds=(-10, 10),
+                      ch_length=16,
+                      population_size=50,
+                      mute_rate=0.01,
+                      n_iters=100)
+
+best_sol, best_fitness = ga.fit()
+print("最优解: x =", best_sol, ", f(x) =", best_fitness)  # 打印最优解
